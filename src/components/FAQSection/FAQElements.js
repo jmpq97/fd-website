8cf3aec215a7d6e424e06de3b16bc758e93a55c2
RiceDaddy321 <0carlitos2@gmail.com>
2022-10-03 01:00:06 -0500

    added linear gradient to questions in the FAQ section.

--- src/components/FAQSection/FAQElements.js
+++ src/components/FAQSection/FAQElements.js
@@ -23,14 +23,12 @@ justify-content: center;
 `;
 
 export const InfoRow = styled.div`
-display: grid;
-grid-auto-columns: minmax(auto, 1fr);
-align-items: center;
-grid-template-areas: ${({ imgStart }) => imgStart ? `'col2 col1'` : `'col1 col2'`};
-
-@media screen and (max-width: 768px) {
-    grid-template-areas: ${({ imgStart }) => imgStart ? `'col1' 'col2'` : `'col1 col1' 'col2 col2'`};
-}
+display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    align-content: center;
+    justify-content: center;
+    align-items: center
 `;
 
 export const Column1 = styled.div`
@@ -50,27 +48,44 @@ margin-left: 60%;
 `
 
 export const TextWrapper = styled.div`
-max-width: 540px;
-padding-top: 0;
-padding-bottom: 60px;
+text-align: center;
+display: inline-flex;
+background: #010606;
+justify-content: center;
+align-items: flex-end;
 `;
 
 export const TopLine = styled.p`
-color: #01bf71;
-font-size: 16px;
-line-height: 16px;
+background: linear-gradient(90deg, #3FA1CB 18.08%, #9D57E3 95.76%);
+-webkit-background-clip: text;
+-webkit-text-fill-color: transparent;
+background-clip: text;
+text-fill-color: transparent;
+ont-family: 'DM Sans';
+font-style: normal;
 font-weight: 700;
-letter-spacing: 1.4px;
-text-transform: uppercase;
-margin-bottom: 16px;
+font-size: 25px;
+line-height: 100%;
+`;
+
+export const BottomLine = styled.li`
+font-family: 'DM Sans';
+font-style: normal;
+font-weight: 400;
+font-size: 20px;
+line-height: 100%;
+/* or 20px */
+
+color: #F9F9F9;
 `;
 
 export const Heading = styled.h2`
-margin-bottom: 24px;
-font-size: 75px;
-line-height: 1.1;
-font-weight: 600;
-color: ${({ lightText }) => (lightText ? '#f7f8fa' : '#010606')};
+font-family: 'DM Sans';
+font-style: normal;
+font-weight: 700;
+font-size: 40px;
+line-height: 100%;
+color: ${({ lightText }) => (lightText ? '#FFFFFF' : '#010606')};
 
 @media screen and (max-width: 480px) {
     font-size: 32px;
@@ -100,3 +115,9 @@ width: 100%;
 margin: 0 0 10px 0;
 padding-right: 0;
 `;
+
+export const LiWrap = styled.div`
+height: auto;
+margin-bottom: 62px;
+width: 500px;
+`
--- src/components/FAQSection/index.js
+++ src/components/FAQSection/index.js
@@ -1,16 +1,18 @@
 import React from 'react'
-import { InfoContainer, InfoWrapper, InfoRow, Column1, Column2, TextWrapper, TopLine, Heading, Subtitle, BtnWrap, ImgWrap, BgElement } from './FAQElements'
+import { InfoContainer, InfoWrapper, InfoRow, Column1, Column2, TextWrapper, TopLine, Heading, Subtitle, BtnWrap, ImgWrap, BgElement, BottomLine, LiWrap } from './FAQElements'
 
 import { FAQ } from './Data'
 
 function ListItem(props) {
     return <>
-    <TopLine>{props.question}</TopLine>
-        <li>{props.answer}</li>
+        <LiWrap>
+            <TopLine>{props.question}</TopLine>
+            <BottomLine>{props.answer}</BottomLine>
+        </LiWrap>
     </>;
 }
 
-function FAQSection({lightBg, id, lightText, imgStart}) {
+function FAQSection({ lightBg, id, lightText, imgStart }) {
     var FAQobj = FAQ;
     const FAQArray = FAQobj.array;
 
@@ -23,14 +25,12 @@ function FAQSection({lightBg, id, lightText, imgStart}) {
                     </TextWrapper>
                     <InfoRow imgStart={imgStart}>
                         <Column1>
-                            <ul>
                                 {FAQArray.slice(0, FAQArray.length / 2).map((QA) => <ListItem key={QA.id} question={QA.question} answer={QA.answer} />)}
-                            </ul>
+                           
                         </Column1>
                         <Column2>
-                            <ul>
                                 {FAQArray.slice(FAQArray.length / 2).map((QA) => <ListItem key={QA.id} question={QA.question} answer={QA.answer} />)}
-                            </ul>
+                        
                         </Column2>
                     </InfoRow>
                 </InfoWrapper>
